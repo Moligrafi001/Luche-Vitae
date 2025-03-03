6807a9cfc8a3addf94f51e9bf1e174e91a5637dc
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2025-03-03 00:34:34 -0300

    update Auth.lua

--- Auth.lua
+++ Auth.lua
@@ -1,8 +1,9 @@
 local LucheVitae = setmetatable(
   {},
   {
-    __newindex = function(self, key)
-      return rawget(self, key)
+    __newindex = function(self, key, value)
+      print("Nova chave adicionada:", key, "com o valor:", value)
+      rawset(self, key, value)
     end,
     __metatable = false
   }
