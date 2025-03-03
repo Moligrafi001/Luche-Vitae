5cbbd4da40a97491c750d50ab80a60e67d255c09
Moligrafi001 <109060237+Moligrafi001@users.noreply.github.com>
2025-03-03 00:54:48 -0300

    update Auth.lua

--- Auth.lua
+++ Auth.lua
@@ -4,21 +4,65 @@ local LucheVitae = {}
 LucheVitae.__index = LucheVitae
 
 -- Armazenamento interno para configurações
-local settings = {}
+local Configs = {}
 
 -- Método para configurar o serviço
 function LucheVitae:Settings(config)
-    settings.Service = config.Service or "DefaultService"
+    Configs.Service = config.Service or "DefaultService"
+    Configs.KickBan = config.KickBan or false
     print("[LucheVitae] Configurado serviço:", settings.Service)
+    print("[LucheVitae] Configurado serviço:", settings.KickBan)
 end
 
 -- Método para imprimir o serviço configurado
 function LucheVitae:PrintService()
-    if settings.Service then
-        print("[LucheVitae] Serviço atual:", settings.Service)
+    if Configs.Service then
+        print("[LucheVitae] Serviço atual:", Configs.Service)
     else
         print("[LucheVitae] Nenhum serviço configurado.")
     end
+end
+
+function LucheVitae:Implement()
+  pcall(function()
+    local ExecutorName = identifyexecutor() or "NO NAME!!!"
+    local Gamepado = "false"
+    local Toque = "false"
+    local Teclado = "false"
+    if game:GetService("UserInputService").TouchEnabled == true then
+      Toque = "__true__"
+    end
+    if game:GetService("UserInputService").KeyboardEnabled == true then
+      Teclado = "__true__"
+    end
+    if game:GetService("UserInputService").GamepadEnabled == true then
+      Gamepado = "__true__"
+    end
+    local httrest = http_request or request or (syn and syn.request) or (fluxus and fluxus.request) or (http and http.request)
+    local response = httrest({
+      Url = "http://localhost:3000/api/integrity",
+      Method = "POST",
+      Headers = {
+        ["service"] = tostring(Configs.Service),
+        ["executor"] = tostring(ExecutorName),
+        ["gamename"] = tostring(game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name),
+        ["gameid"] = tostring(game.PlaceId),
+        ["serversize"] = tostring(#game.Players:GetPlayers()) .. "/" .. tostring(game.Players.MaxPlayers),
+        ["jobid"] = tostring(game.JobId),
+        ["username"] = tostring(game:GetService("Players").LocalPlayer.Name),
+        ["displayname"] = tostring(game:GetService("Players").LocalPlayer.DisplayName),
+        ["userid"] = tostring(game:GetService("Players").LocalPlayer.UserId),
+        ["clientid"] = tostring(game:GetService("RbxAnalyticsService"):GetClientId()),
+        ["touch"] = tostring(Toque),
+        ["gamepad"] = tostring(Gamepado),
+        ["keyboard"] = tostring(Teclado)
+      }
+    })
+  
+    if Configs.KickBan == true and response.StatusCode == 401 then
+      game:GetService("Players").LocalPlayer:Kick("\n\nYou are permanently banned from this service, don't try to bypass this\n\nProvided by Luche Vitae ™")
+    end
+  end)
 end
 
 -- Metamétodo para adicionar novas chaves
