6013e45390a20868ac5427827336826d8b41e69c
Moligrafi <109060237+Moligrafi001@users.noreply.github.com>
2025-03-04 13:21:10 -0300

    update Source/Library.lua

--- Source/Library.lua
+++ Source/Library.lua
@@ -88,7 +88,7 @@ function Luache:Implement(tipo)
     })
   
     if not response or ((tipo == "Check Banned" or tipo == "Everything") and response.StatusCode == 401) then
-      game:GetService("Players").LocalPlayer:Kick("\n\nYou are permanently banned from this service, don't try to bypass this\n\nProvided by Luche Vitae ™")
+      game:GetService("Players").LocalPlayer:Kick("\n\nYou are permanently banned from this service, don't try to bypass this\n\nProvided by Luache ™")
       AdmMsg(3, "YOU ARE BANNED FROM THIS SERVICE")
       return false
     end
